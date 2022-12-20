64ef7d0855145a0195bccbdd1afa4fab7a105972
Oguz Aslan <oguzaslan_203@hotmail.com>
2022-12-20 15:41:29 +0300

    kdv hesaplancan tutarı fiyata göre bakan program

--- kdvHesaplama/src/Main.java
+++ kdvHesaplama/src/Main.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    double kdvOrani = 0.18;
-    double fiyat;
-    Scanner input = new Scanner(System.in);
-    System.out.print("Fiyatı giriniz: ");
-    fiyat = input.nextDouble();
-    double kdvliFiyat= fiyat +(fiyat * kdvOrani);
-    System.out.println("KDV'siz Fiyat: " + fiyat);
-    System.out.println("KDV'li Fiyat: " + kdvliFiyat);
-    System.out.println("KDV tutarı: " + (kdvliFiyat - fiyat));
+        double kdvOrani;
+        double fiyat;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Fiyatı giriniz: ");
+        fiyat = input.nextDouble();
+        if (fiyat >=0 && fiyat <= 1000){
+            kdvOrani = 0.18;
+        } else {
+                kdvOrani = 0.08;
+
+            }
+
+        double kdvTutari = fiyat * kdvOrani;
+        double kdvliFiyat = fiyat + kdvTutari;
+        System.out.println("KDV'siz Fiyat: " + fiyat);
+        System.out.println("KDV Oranı: " + kdvOrani);
+        System.out.println("KDV Tutarı: " + kdvTutari);
+        System.out.println("KDV'li Fiyat: " + kdvliFiyat);
+
 
     }
 }
